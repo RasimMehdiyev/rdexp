fd4625cbacfe8fe7daacfb79410596db7890c628
Rasim Mehdiyev <rasim.mehdiyev@student.kuleuven.be>
2023-10-23 12:46:43 +0200

    updated supabase for production

--- src/lib/helper/supabaseClient.js
+++ src/lib/helper/supabaseClient.js
@@ -1,8 +1,14 @@
-import { createClient } from "@supabase/supabase-js";
+// import { createClient } from "@supabase/supabase-js";
 
 
-export const supabase = createClient(
-    String(process.env.REACT_APP_SUPABASE_URL),
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5lZWl4c3RqeHl4ZGJxdWViZ2tjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTU5OTQ1MjksImV4cCI6MjAxMTU3MDUyOX0.S9NLt_yf96o7gaNmNLYQiXqekZ2M55QhxYUWXeqZn5g"
-)
+// export const supabase = createClient(
+//     String(process.env.REACT_APP_SUPABASE_URL),
+//     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5lZWl4c3RqeHl4ZGJxdWViZ2tjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTU5OTQ1MjksImV4cCI6MjAxMTU3MDUyOX0.S9NLt_yf96o7gaNmNLYQiXqekZ2M55QhxYUWXeqZn5g"
+// )
 
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;  
+const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;  
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
