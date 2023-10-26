28e073f1a1c4971ab0e7c7d1165bf16681accf4a
Maxime Bellis <maxime.bellis@student.kuleuven.be>
2023-10-26 14:19:01 +0200

    Made test folder, testing actions

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,5 +1,7 @@
+process.env.REACT_APP_SUPABASE_URL = 'mock_supabase_url';
+
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from '../App';
 
 test('renders learn react link', () => {
   render(<App />);
