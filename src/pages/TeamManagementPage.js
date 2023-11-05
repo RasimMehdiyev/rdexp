0ac0b51030adb7b67107d7742cf75213c29b8d76
Penelope Rekkas <penelope.rekkas@student.kuleuven.be>
2023-11-05 19:07:17 +0100

    Plus button for players added

--- src/pages/TeamManagementPage.js
+++ src/pages/TeamManagementPage.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTrash } from "@fortawesome/free-solid-svg-icons";
 import { faWindowClose } from "@fortawesome/free-solid-svg-icons";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
 
 const TeamManagementPage = () => {
     const [isPlayerClicked, setPlayerClicked] = useState(false);
@@ -24,18 +24,20 @@ const TeamManagementPage = () => {
             <div style={{ marginLeft:"2rem"}}>
                 <h2 className="text-5xl text-blue-700 mb-5 mt-10">Players</h2>
                 <div className="flex items-center mb-5">
-                    <div className="circle-number bg-orange-500 text-white mr-10">21</div>
-                    <h5 className={`font-interReg cursor-pointer ${isPlayerClicked ? "bg-blue-200 rounded-lg p-2 pl-5 pr-60" : ""}`}
+                    <div className="circle-number bg-orange-500 text-white mr-8">21</div>
+                    <h5 className={`font-interReg cursor-pointer ${isPlayerClicked ? "bg-blue-200 rounded-lg p-2 pl-5 pr-40" : ""}`}
                     style={{ fontSize: "2rem" }}
                     onClick={togglePlayerBackground} > 
                     Michael Johnson
                     </h5>
                     {isPlayerClicked && (
                     <span className="ml-2 text-blue-200"> {/* Adjust color as needed */}
-                    <FontAwesomeIcon icon={faWindowClose} className="pl-5 fa-4x"/> {/* Adjust size as needed */}
+                    <FontAwesomeIcon icon={faWindowClose} className="pl-3 fa-4x"/> {/* Adjust size as needed */}
                     </span>)}
-
                 </div>
+                <button className="bg-blue-700 text-white rounded-lg mt-5 pt-5 pb-5 pl-72 pr-72 cursor-pointer">
+                <FontAwesomeIcon icon={faPlus} className="ml-2 fa-2x" /> {/* Add plus icon */}
+                </button>
             </div>
 
         </div>
