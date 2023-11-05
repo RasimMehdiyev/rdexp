5a2b855ac3ce382ce496ce022e124f294cc6bd97
Penelope Rekkas <penelope.rekkas@student.kuleuven.be>
2023-11-05 20:08:17 +0100

    All components added to TeamManagement

--- src/pages/TeamManagementPage.js
+++ src/pages/TeamManagementPage.js
@@ -4,12 +4,52 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faWindowClose } from "@fortawesome/free-solid-svg-icons";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
 
+const ClickablePerson = ({ name, isClicked, onClick }) => (
+    <h5
+      className={`font-interReg cursor-pointer ${
+        isClicked ? "bg-blue-200 rounded-lg p-2 pl-5 pr-40" : ""
+      }`}
+      style={{ fontSize: "2rem" }}
+      onClick={onClick}
+    >
+      {name}
+    </h5>
+);
+
+const CloseIcon = () => (
+    <span className="ml-2 text-blue-200">
+      <FontAwesomeIcon icon={faWindowClose} className="pl-3 fa-4x" />
+    </span>
+);
+
 const TeamManagementPage = () => {
     const [isPlayerClicked, setPlayerClicked] = useState(false);
 
     const togglePlayerBackground = () => {
         setPlayerClicked(!isPlayerClicked);
     };
+
+    const [isPersonClicked, setPersonClicked] = useState(false);
+
+    const togglePersonBackground = () => {
+        setPersonClicked(!isPersonClicked);
+    };
+
+    const [isRoleClicked, setRoleClicked] = useState(false);
+
+    
+    const toggleRoleBackground = () => {
+        setRoleClicked(!isRoleClicked);
+    }
+
+
+    const headerClass = "text-5xl text-blue-700 mb-5 mt-10";
+
+    const plusButton = (
+        <button className="bg-blue-500 text-white rounded-lg mt-5 pt-5 pb-5 pl-72 pr-72 cursor-pointer">
+          <FontAwesomeIcon icon={faPlus} className="ml-2 fa-2x" />
+        </button>
+      );
 
     return (
         <div className="h-screen"> 
@@ -22,22 +62,39 @@ const TeamManagementPage = () => {
             </div>
 
             <div style={{ marginLeft:"2rem"}}>
-                <h2 className="text-5xl text-blue-700 mb-5 mt-10">Players</h2>
+                <h2 className={headerClass}>Players</h2>
                 <div className="flex items-center mb-5">
                     <div className="circle-number bg-orange-500 text-white mr-8">21</div>
-                    <h5 className={`font-interReg cursor-pointer ${isPlayerClicked ? "bg-blue-200 rounded-lg p-2 pl-5 pr-40" : ""}`}
-                    style={{ fontSize: "2rem" }}
-                    onClick={togglePlayerBackground} > 
-                    Michael Johnson
-                    </h5>
-                    {isPlayerClicked && (
-                    <span className="ml-2 text-blue-200"> {/* Adjust color as needed */}
-                    <FontAwesomeIcon icon={faWindowClose} className="pl-3 fa-4x"/> {/* Adjust size as needed */}
-                    </span>)}
+                    <ClickablePerson
+                    name="Michael Johnson"
+                    isClicked={isPlayerClicked}
+                    onClick={togglePlayerBackground}
+                    />
+                    {isPlayerClicked && <CloseIcon />}
                 </div>
-                <button className="bg-blue-700 text-white rounded-lg mt-5 pt-5 pb-5 pl-72 pr-72 cursor-pointer">
-                <FontAwesomeIcon icon={faPlus} className="ml-2 fa-2x" /> {/* Add plus icon */}
-                </button>
+                {plusButton}
+                <h2 className={headerClass}>Extras</h2>
+                <div className="flex items-center mb-5">
+                    <ClickablePerson
+                        name="Jessica Lee"
+                        isClicked={isPersonClicked}
+                        onClick={togglePersonBackground}
+                    />
+                    {isPersonClicked && <CloseIcon />}
+                </div>
+                {plusButton}
+
+                <h2 className={headerClass}>Extra roles needed</h2>
+                <div className="flex items-center mb-5">
+                    <ClickablePerson
+                        name="Referee"
+                        isClicked={isRoleClicked}
+                        onClick={toggleRoleBackground}
+                    />
+                    {isRoleClicked && <CloseIcon />}
+                </div>
+                {plusButton}
+
             </div>
 
         </div>
