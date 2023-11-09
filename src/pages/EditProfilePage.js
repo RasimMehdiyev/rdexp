f1e29d2913794645bd4a26209f15baecf47e936e
yeskamuhammad.omar <yeskamuhammad.omar@student.kuleuven.be>
2023-11-10 00:43:21 +0100

    now the placeholder for inputs in edit profile pages are added in dynamically and you can also type in there

--- src/pages/EditProfilePage.js
+++ src/pages/EditProfilePage.js
@@ -1,20 +1,84 @@
 import React from "react";
+import { useEffect, useState } from 'react';
+import { supabase } from '../lib/helper/supabaseClient';
+import LoadingPage from "./LoadingPage";
 import { PencilIcon } from '@heroicons/react/24/solid'
 import { Link, useNavigate } from 'react-router-dom';
 
 const EditProfilePage = () => {
-  return (
+    const [userData, setUserData] = useState({});
+    
+    const [role, setRole] = useState('');
+
+    const [newBio, setNewBio] = useState('');
+    const [newEmail, setNewEmail] = useState('');
+    const [newPhoneNumber, setNewPhoneNumber] = useState('');
+
+    const [loading, setLoading] = useState(true); // Add a loading state
+    // navigate
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const fetchData = async () => {
+          console.log("Fetching data");
+          try {
+            const userResponse = await supabase.auth.getUser();
+            console.log("User:", userResponse);
+            const user = userResponse.data.user;
+            if (user) {
+              // Initially, we don't know the user's role, so fetch from both tables.
+              const { data: coachData, error: coachError } = await supabase
+                .from('Coach')
+                .select('*')
+                .eq('userID', user.id)
+                .single(); // Use single to get a single record or null
+    
+              if (coachData) {
+                setUserData(coachData);
+                setRole("Coach");
+              } else {
+                console.log("Coach Error:", coachError);
+                const { data: playerData, error: playerError } = await supabase
+                  .from('Player')
+                  .select('*')
+                  .eq('userID', user.id)
+                  .single(); // Use single to get a single record or null
+    
+                if (playerData) {
+                  setUserData(playerData);
+                  setRole("Player");
+                } else {
+                  console.log("Player Error:", playerError);
+                }
+              }
+            }
+          } catch (error) {
+            console.error("Error fetching data:", error);
+          }
+          finally {
+            setLoading(false); // Stop loading regardless of the outcome
+          }
+        };
+    
+        fetchData();
+      }, []);
+
+    return (
     <div className="grow flex bg-indigo-100 flex-col items-center justify-start h-screen">
-      <div className="grow p-4 flex-col justify-start items-center gap-4 inline-flex">
+        <div className="grow p-4 flex-col justify-start items-center gap-4 inline-flex">
         <div className="w-[329px] justify-center items-start gap-2.5 inline-flex">
             <div className="w-[142px] h-[142px] relative">
-                <img className="w-[142px] h-[142px] left-0 top-0 absolute rounded-full border-4 border-white" src="https://via.placeholder.com/142x142" />
-                <div className="w-[35px] h-[35px] left-[107px] top-[98px] absolute">
-                     
+                {userData.profilePicture ? (
+                    <img className="w-[142px] h-[142px] rounded-full border-4 border-white" src={userData.profilePicture} />
+
+                ) : (
+                    <img className="w-[142px] h-[142px] rounded-full border-4 border-white" src={process.env.PUBLIC_URL + "/images/no_user.png"} />
+                )}                      
+                
+                <div className="w-[35px] h-[35px] left-[107px] top-[98px] absolute">                        
                     <div className="w-[35px] h-[35px] left-[-3px] top-0 absolute bg-zinc-300 rounded-full flex justify-center items-center">
                     <PencilIcon className="h-6 w-6" />   
-                    </div>
-                      
+                    </div>                        
                 </div>
             </div>
         </div>
@@ -23,9 +87,14 @@ const EditProfilePage = () => {
                 <div className="w-[178px] px-4 justify-start items-start gap-2.5 inline-flex">
                     <div className="text-black text-base font-normal font-interReg leading-normal">About yourself</div>
                 </div>
-                <div className="w-[322px] px-4 py-1 bg-white rounded-md border border-orange-500 justify-start items-start inline-flex">
-                    <div className="grow shrink basis-0 h-[68px] justify-start items-center flex">
-                        <div className="grow shrink basis-0 text-neutral-500 text-sm font-normal font-interReg">Hey there, I am Jeff, I love football and I’ve been in the coaching game for the past 15 years. I’m just happy to be here! If you need anything don’t hesitate to contact me :)</div>
+                <div className="w-[322px] px-4 py-1 bg-white rounded-md border border-orange-500 justify-start items-center inline-flex">
+                    <div className="grow h-auto basis-0 justify-start items-center flex">
+                        <textarea
+                            value={newBio}
+                            onChange={(event) => setNewBio(event.target.value)}
+                            className="grow basis-0 text-neutral-500 text-sm font-normal font-interReg"
+                            placeholder={userData.bio}
+                            />
                     </div>
                 </div>
             </div>
@@ -34,8 +103,13 @@ const EditProfilePage = () => {
                     <div className="text-black text-base font-normal font-interReg leading-normal">Email</div>
                 </div>
                 <div className="w-[322px] h-8 pl-5 pr-4 py-3 bg-white rounded-md border border-orange-500 justify-start items-center gap-2.5 inline-flex">
-                    <div className="grow shrink basis-0 h-6 justify-start items-center gap-[116px] flex">
-                        <div className="text-neutral-500 text-base font-normal font-interReg leading-normal">jeff.hugh@email.co</div>
+                    <div className="grow h-auto basis-0 justify-start items-center flex">
+                        <input
+                            className='text-neutral-500 text-base font-normal font-interReg leading-normal'
+                            placeholder={userData.email}
+                            type="email"
+                            value={newEmail}
+                            onChange={(event) => setNewEmail(event.target.value)} />
                     </div>
                 </div>
             </div>
@@ -44,8 +118,13 @@ const EditProfilePage = () => {
                     <div className="text-black text-base font-normal font-interReg leading-normal">Phone number</div>
                 </div>
                 <div className="w-[322px] h-8 pl-5 pr-4 py-3 bg-white rounded-md border border-orange-500 justify-start items-center gap-2.5 inline-flex">
-                    <div className="grow shrink basis-0 h-6 justify-start items-center gap-[116px] flex">
-                        <div className="text-neutral-500 text-base font-normal font-interReg leading-normal">+12 345 567 89</div>
+                    <div className="grow h-auto basis-0 justify-start items-center flex">                            
+                        <input
+                            className="text-neutral-500 text-base font-normal font-interReg leading-normal"
+                            placeholder={userData.phoneNumber}
+                            type="tel"
+                            value={newPhoneNumber}
+                            onChange={(event) => setNewPhoneNumber(event.target.value)} />
                     </div>
                 </div>
             </div>
@@ -60,9 +139,9 @@ const EditProfilePage = () => {
             </div>
             </Link>
         </div>
-      </div>
+        </div>
     </div>  
-  );
+    );
 }
 
 export default EditProfilePage;
