d823fcc90dcddcad2fe2e146d913fc6fbafa490e
Penelope Rekkas <penelope.rekkas@student.kuleuven.be>
2023-11-28 17:34:53 +0100

    GameSettingsPage in progress

--- src/pages/GameSettings.js
+++ src/pages/GameSettings.js
@@ -5,31 +5,29 @@ import {Link} from 'react-router-dom'
 
 const GameSettings = () => {
   return (
-    <div className=" h-screen  bg-sn-bg-light-blue">
+    <div className="h-screen flex flex-col justify-between bg-sn-bg-light-blue">
 
-      <div className='text-center'>
-        <h1 className="pt-20 text-5xl text-game-blue">
-          GAME
-        </h1>
-        <h1 className="pt-2 text-5xl text-game-blue">
-          SETTINGS
-        </h1>
-    
-      </div>
+    <div>
+        <div className='text-center'>
+            <h1 className="pt-20 text-5xl text-game-blue">
+            GAME
+            </h1>
+            <h1 className="pt-2 text-5xl text-game-blue">
+            SETTINGS
+            </h1>
+        </div>
 
-      <div className='pl-5'>
-        <h1 className="pt-7 pb-4 text-3xl text-game-blue">
-            Add roles
-        </h1>
+        <div className='pl-5'>
+            <h1 className="pt-7 pb-4 text-3xl text-game-blue">
+                Add roles
+            </h1>
+            <PersonTag name="Referee" number="null" isPlayer={false} />
+            <RoleInput/>
+        </div>
+    </div>
 
-        <PersonTag name="Referee" number="null" isPlayer={false} />
-        <RoleInput/>
 
-        
-
-      </div>
-
-      <div className="bg-sn-bg-light-blue flex flex-col justify-center align-items text-center pt-14 pl-[15%]">
+      <div className="bg-sn-bg-light-blue flex flex-col   align-items text-center pl-[15%]">
         <button className="bg-sn-main-orange text-2xl text-white font-interElight p-2 rounded-10px w-[70vw] h-16 ">
           SAVE
         </button>
@@ -37,7 +35,6 @@ const GameSettings = () => {
         <Link className=" text-game-blue pt-10 underline underline-offset- font-interElight p-2 rounded-10px w-[70vw] h-12 pb-10 ">
           skip this step
         </Link>
-
       </div>
 
     </div>
