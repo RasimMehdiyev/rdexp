62de7362cae801ceac0e01fa7bf77c30ec5e5ef1
Penelope Rekkas <penelope.rekkas@student.kuleuven.be>
2023-11-28 18:02:38 +0100

    PersonTag edited

--- src/components/PersonTag.js
+++ src/components/PersonTag.js
@@ -1,20 +1,25 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrashAlt } from "@fortawesome/free-solid-svg-icons";
 import PlayerDeletionModal from '../components/PlayerDeletionModal.js';
 
-const PersonTag = ({ name, number, isPlayer }) => {
+const PersonTag = ({ name, number, isPlayer, isMember }) => {
 
-    const [isDeletionModalOpen, setDeletionModalOpen] = useState(false);
+//isMember being true means trashIcon is pressed for a player 
+//that's already part of that team (i.e. from the team management page) 
+//which results in the confirmation modal popping up
+//isMember being false means the trashIcon is pressed for a player 
+//that isn't part of that team (i.e. from the team creation page) 
+//and hence can't be removed from display without confirmation
+  const [isDeletionModalOpen, setDeletionModalOpen] = useState(false);
 
-    const openDeletionModal = () => {
-        setDeletionModalOpen(true);
-      };
-    
-      const closeDeletionModal = () => {
-        setDeletionModalOpen(false);
-      };
+  const openDeletionModal = () => {
+    setDeletionModalOpen(true);
+  };
+
+  const closeDeletionModal = () => {
+    setDeletionModalOpen(false);
+  };
 
   return (
     <div className="flex items-center mb-5 bg-white shadow-md p-2 rounded-10px w-[90vw]">
@@ -22,11 +27,10 @@ const PersonTag = ({ name, number, isPlayer }) => {
         <div className="font-russoOne circle-number shadow-md bg-sn-main-orange text-white mr-8">{number}</div>
       )}
       <h5 className="font-interReg text-2xl cursor-pointer">{name}</h5>
-      <FontAwesomeIcon icon={faTrashAlt} className="ml-auto pr-2 text-game-blue text-2xl" onClick={openDeletionModal} />
+      <FontAwesomeIcon icon={faTrashAlt} className="ml-auto pr-2 text-game-blue text-2xl" onClick={isMember ? openDeletionModal : undefined} />
 
-      <PlayerDeletionModal isOpen={isDeletionModalOpen} closeModal={closeDeletionModal} />
+      {isMember && <PlayerDeletionModal isOpen={isDeletionModalOpen} closeModal={closeDeletionModal} />}
     </div>
-    
   );
 };
 
