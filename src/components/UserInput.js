6052033eb24f2d7725a3d38f96f06d0ea0450951
Penelope Rekkas <penelope.rekkas@student.kuleuven.be>
2023-11-28 15:26:54 +0100

    TeamCreatePage in progress

---
+++ src/components/UserInput.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser, faPlus } from '@fortawesome/free-solid-svg-icons';
+
+const UserInput = () => {
+  return (
+    <div className="flex items-center w-[90vw] pt-2">
+      <div className="relative">
+        <input
+          className="h-[6vh] pl-10 w-[80vw] rounded-10px border-2 border-game-blue font-interReg placeholder-text"
+          placeholder="Enter user's name"
+        />
+        <div className="absolute left-3 top-2 pt-1">
+          <FontAwesomeIcon icon={faUser} className="text-game-blue h-[3vh]" />
+        </div>
+      </div>
+
+      <button className="bg-game-blue p-2 pl-3 pr-3 rounded-10px ml-auto">
+        <FontAwesomeIcon icon={faPlus} className="text-white" />
+      </button>
+    </div>
+  );
+};
+
+export default UserInput;
--- src/pages/TeamCreatePage.js
+++ src/pages/TeamCreatePage.js
@@ -3,6 +3,7 @@ import { faUser} from "@fortawesome/free-regular-svg-icons";
 import { faPlus} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import PersonTag from '../components/PersonTag.js';
+import UserInput from '../components/UserInput.js';
 
 const TeamCreatePage = () => {
   return (
@@ -15,7 +16,7 @@ const TeamCreatePage = () => {
         <h1 className="pt-2 text-5xl text-game-blue">
           YOUR TEAM
         </h1>
-        <input className="mt-5 h-[5vh] pl-2 w-[70vw] rounded-10px border-2 border-game-blue font-interReg" 
+        <input className="mt-5 h-[5vh] pl-2 w-[70vw] rounded-10px border-2 border-game-blue font-interReg placeholder-text" 
         placeholder="Team name"/>
       </div>
 
@@ -27,23 +28,9 @@ const TeamCreatePage = () => {
         <PersonTag name="Mick Johnson" number="21" isPlayer={true} />
         <PersonTag name="Josh Davis" number="36" isPlayer={true} />
 
-        <div className="flex items-center w-[90vw]">
+        
 
-          <div className="relative">
-            <input
-              className="h-[6vh] pl-10 w-[80vw] rounded-10px border-2 border-game-blue font-interReg"
-              placeholder="Enter user's name"
-            />
-            <div className="absolute left-3 top-2 pt-1">
-              <FontAwesomeIcon icon={faUser} className="text-game-blue h-[3vh]" />
-            </div>
-          </div>
-
-          <button className=" bg-game-blue p-2 pl-3 pr-3 rounded-10px  ml-auto">
-              <FontAwesomeIcon icon={faPlus} className="text-white" />
-          </button>
-
-        </div>
+        <UserInput />
                     
       </div>
 
