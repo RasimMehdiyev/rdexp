81063c0df9c5587b0179e45911b129b20c4353f7
yeskamuhammad.omar <yeskamuhammad.omar@student.kuleuven.be>
2023-11-30 10:55:51 +0100

    sticky subheader looks for profile and edit profile okay

--- src/components/StickyEditProfileComponent.js
+++ src/components/StickyEditProfileComponent.js
@@ -15,10 +15,10 @@ const StickyEditProfileComponent = () => {
       <div className="p-2 h-16 flex flex-row justify-between items-center">
         <div className='flex flex-row justify-between gap-1 items-center'>
             <img className='cursor-pointer' onClick={getPreviousPage} src={process.env.PUBLIC_URL + "/images/chevron-down.svg"} alt="" />
-            <p className='text-[20px] font-russoOne text-white'>Game</p>
+            <p className='text-[20px] font-russoOne text-white'>Edit Profile</p>
         </div>
         <div className='flex flex-row justify-between gap-4'>       
-            <button className='bg-sn-main-blue h-8 w-[72px] text-white rounded-[10px] text-[14px]'>EDIT</button>
+            <button className='bg-sn-main-orange h-8 w-[72px] text-white rounded-[10px] text-[14px]'>SAVE</button>
         </div>
       </div>
     </div>
---
+++ src/components/StickySubheaderProfileComponent.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+
+
+const StickySubheaderProfileComponent = () => {
+    const nagivate = useNavigate();
+
+    const getPreviousPage = () => {
+        nagivate(-1);
+    }
+
+
+  return (
+    <div className="bg-sn-subheader-blue sticky top-1 shadow-md z-10"> 
+      <div className="p-2 h-16 flex flex-row justify-between items-center">
+        <div className='flex flex-row justify-between gap-1 items-center'>
+            <img className='cursor-pointer' onClick={getPreviousPage} src={process.env.PUBLIC_URL + "/images/chevron-down.svg"} alt="" />
+            <p className='text-[20px] font-russoOne text-white'>Profile</p>
+              </div>
+        <Link to="editProfile/">
+        <div className='flex flex-row justify-between gap-4'>       
+            <button className='bg-sn-main-blue h-8 w-[72px] text-white rounded-[10px] text-[14px]'>EDIT</button>
+                  </div>
+                  </Link>
+      </div>
+    </div>
+  );
+}
+
+export default StickySubheaderProfileComponent
