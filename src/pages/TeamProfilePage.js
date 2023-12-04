4facfc64fdb4cdd0c39a713c4043dc2472512f57
yeskamuhammad.omar <yeskamuhammad.omar@student.kuleuven.be>
2023-12-04 23:11:21 +0100

    fixed deployment problem, npm run build now works

--- src/pages/TeamProfilePage.js
+++ src/pages/TeamProfilePage.js
@@ -202,7 +202,7 @@ const TeamProfilePage = () => {
     }  
     else{
         return (
-        <div className="w-screen h-screen py-4 bg-gradient-to-b from-indigo-100 via-white to-white flex-col justify-start items-center flex">
+        <div className="w-screen h-screen py-4 flex-col justify-start items-center flex">
             <TeamProfileHeaderComponent
                 clubData={clubData}
                 teamData={teamData} 
