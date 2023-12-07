63b6385773e7e99f43864e65eea5d20730a3538b
yeskamuhammad.omar <yeskamuhammad.omar@student.kuleuven.be>
2023-12-07 13:02:11 +0100

    added functions for later accept and decline button handlings

--- src/pages/NotificationPage.js
+++ src/pages/NotificationPage.js
@@ -12,14 +12,13 @@ const NotificationPage = () => {
     useEffect(() => {
         const fetchData = async () => {
             setLoading(true);
-
             try {
                 const userResponse = await supabase.auth.getUser();
                 console.log("User:", userResponse);
                 const user = userResponse.data.user;
                 
                 if (user) {                     
-                    let { teamInviteData, teamInviteError } = await supabase
+                    let { data: teamInviteData, teamInviteError } = await supabase
                         .rpc('get_team_user_invites', {
                             param_user_id: user.id
                         })
@@ -44,7 +43,7 @@ const NotificationPage = () => {
 
             } catch (error) {
                 console.error(error);
-                alert("Error happened, please reload the page.");
+                alert("Error happened, please reload the page.");                
             } finally {
                 setLoading(false);
             }
@@ -54,6 +53,42 @@ const NotificationPage = () => {
         fetchData();
     }, []);
 
+    const updateTeamInviteLoadingStatus = (id, loadingStatus) => {
+        setTeamInvites(prevInvites => {
+            return prevInvites.map(invite => {
+                if (invite.id == id) {
+                return { ...invite, loading: loadingStatus };
+                }
+                return invite;
+            });
+        });
+    };
+
+    const updateEventInviteLoadingStatus = (id, loadingStatus) => {
+        setEventInvites(prevInvites => {
+            return prevInvites.map(invite => {
+                if (invite.id == id) {
+                return { ...invite, loading: loadingStatus };
+                }
+                return invite;
+            });
+        });
+    };
+
+    const removeTeamInviteById = (id) => {
+        const updatedInvites = teamInvites.filter((element) => element.id != id);
+        // Optionally, you can set the state or update the array as needed
+        setTeamInvites(updatedInvites);
+        // console.log(updatedElements); // Log the updated array
+    };
+
+    const removeEventInviteById = (id) => {
+        const updatedInvites = eventInvites.filter((element) => element.id != id);
+        // Optionally, you can set the state or update the array as needed
+        setEventInvites(updatedInvites);
+        // console.log(updatedElements); // Log the updated array
+    };
+
     if (loading) {
         return ( <LoadingPage></LoadingPage>)
     } else {
@@ -61,19 +96,25 @@ const NotificationPage = () => {
         <div className='h-full w-full flex flex-col gap-4'>
             <div className='text-xl font-russoOne text-game-blue'>Team invites</div>
             <div className='flex flex-col gap-4'>
-                {teamInvites ?
+                {teamInvites.length > 0 ?
                     teamInvites.map((teamInvite) => (
-                    <div key={teamInvite.id }>
-                        <div className='flex flex-col gap-1'>
-                            <div className='text-sm font-interReg'>{teamInvite.id}</div>
-                            <div className='text-sm font-interReg'>{teamInvite.team_name}</div>
-                            <div className='text-sm font-interReg'>{teamInvite.club_name}</div>
+                        <div key={teamInvite.id}>
+                            {teamInvite.loading ? (<LoadingPage></LoadingPage>) :
+                                (
+                                <div>
+                                    <div className='flex flex-col gap-1 bg-sn-bg-light-blue rounded-md'>
+                                        <div className='text-sm font-interReg'>{teamInvite.team_name}</div>
+                                        <div className='text-sm font-interReg'>{teamInvite.club_name}</div>
+                                    </div>
+                                    <div className='flex flex-row gap-8'>
+                                        <button className='rounded-sm'>Accept</button>
+                                        <button className='rounded-sm'>Decline</button>
+                                    </div>
+                                </div>
+                                )
+                            }
+                        
                         </div>
-                        <div className='flex flex-row gap-8'>
-                            <button>Accept</button>
-                            <button>Decline</button>
-                        </div>
-                    </div>
                     )) :
                     (<div className='text-sm font-interreg'>You have no team invite</div>)
                 }
@@ -81,23 +122,28 @@ const NotificationPage = () => {
             </div>
             <div className='text-xl font-russoOne text-game-blue'>Event invites</div>
             <div className='flex flex-col gap-4'>
-                {eventInvites ?                    
+                {eventInvites.length > 0 ?                    
                     eventInvites.map((eventInvite) => (
-                        <div key={eventInvite.id}>                        
-                        <div className='flex flex-col gap-1 bg-sn-bg-light-blue rounded-md'>                            
-                            <div className='text-sm font-interReg'>{eventInvite.title}</div>
-                                <div className='text-sm font-interReg'>{eventInvite.datetime}</div>
-                                <div className='text-sm font-interReg'>{eventInvite.location}</div>
-                                <div className='text-sm font-interReg'>{eventInvite.type}</div>
-                                <div className='text-sm font-interReg'>{eventInvite.team_name}</div>
-                                {eventInvite.position_name &&
-                                    <div className='text-sm font-interReg'>{eventInvite.position_name}</div>
-                                }
-                        </div>
-                        <div className='flex flex-row gap-8'>
-                            <button>Accept</button>
-                            <button>Decline</button>
-                        </div>
+                        <div key={eventInvite.id}>
+                            {eventInvite.loading ? (<LoadingPage></LoadingPage>) :
+                                (
+                                    <div>
+                                    <div className='flex flex-col gap-1 bg-sn-bg-light-blue rounded-md'>                            
+                                        <div className='text-sm font-interReg'>{eventInvite.title}</div>
+                                            <div className='text-sm font-interReg'>{eventInvite.datetime}</div>
+                                            <div className='text-sm font-interReg'>{eventInvite.location}</div>
+                                            <div className='text-sm font-interReg'>{eventInvite.type}</div>
+                                            <div className='text-sm font-interReg'>{eventInvite.team_name}</div>
+                                            {eventInvite.position_name &&
+                                                <div className='text-sm font-interReg'>{eventInvite.position_name}</div>
+                                            }
+                                    </div>
+                                    <div className='flex flex-row gap-8'>
+                                        <button className='rounded-sm'>Accept</button>
+                                        <button className='rounded-sm'>Decline</button>
+                                    </div>
+                                    </div>
+                            )}                        
                         </div>
                     )
                     ) : (
