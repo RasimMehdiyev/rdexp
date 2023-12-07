a9ecb34e82469438bef7d0cf18ad10219ca68ee5
yeskamuhammad.omar <yeskamuhammad.omar@student.kuleuven.be>
2023-12-07 11:32:20 +0100

    succesfully retrieve data

--- src/pages/NotificationPage.js
+++ src/pages/NotificationPage.js
@@ -6,13 +6,41 @@ const NotificationPage = () => {
     const [teamInvites, setTeamInvites] = useState([]);
     const [eventInvites, setEventInvites] = useState([]);
     const [loading, setLoading] = useState(false);
+    const navigate = useNavigate();
 
     useEffect(() => {
         const fetchData = async () => {
             setLoading(true);
 
             try {
+                const userResponse = await supabase.auth.getUser();
+                console.log("User:", userResponse);
+                const user = userResponse.data.user;
                 
+                if (user) {                     
+                    let { teamInviteData, teamInviteError } = await supabase
+                        .rpc('get_team_user_invites', {
+                            param_user_id: user.id
+                        })
+                    if (teamInviteError) console.error(teamInviteError)
+                    else {
+                        console.log("team invites: ", teamInviteData);
+                        setTeamInvites(teamInviteData);
+                    }
+                    
+                    let { data: eventUserInvites, eventUserInvitesError } = await supabase                    
+                        .rpc('get_user_events', {
+                            param_user_id: user.id
+                        })
+                    if (eventUserInvitesError) console.error(eventUserInvitesError)
+                    else {
+                        console.log("event invites: ", eventUserInvites);
+                        setEventInvites(eventUserInvites);
+                    }
+                } else {
+                    navigate("/auth");
+                }
+
             } catch (error) {
                 console.error(error);
                 alert("Error happened, please reload the page.");
