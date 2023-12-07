1ab150c5c9df32bb4642959f2e4202ac18fead8b
Rasim Mehdiyev <rasim.mehdiyev@student.kuleuven.be>
2023-12-07 20:27:14 +0100

    GameSettings toast notification

--- src/pages/GameSettings.js
+++ src/pages/GameSettings.js
@@ -4,9 +4,37 @@ import RoleInput from '../components/RoleInput.js';
 import {Link} from 'react-router-dom'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const GameSettings = () => {
+  const [roles, setRoles] = useState([]);
+  const navigate = useNavigate();
 
+  const addRole = (roleName) => {
+    // Assuming roles only have names and no additional data for now
+    const newRole = {
+      name: roleName,
+      number: "null", // Adjust as needed
+      isPlayer: false,
+      isMember: true
+    };
+    setRoles([...roles, newRole]);
+  };
+
+  const saveData = () => {
+    try {
+      // Save data to database
+      toast.success('Game settings saved!', { position: "top-center" });
+      setTimeout(() => {
+        console.log("redirecting")
+      navigate('/');
+    }, 3000); 
+    }
+    catch (error) {
+      toast.error('Error saving game settings!', { position: "top-center" });
+    }
+  }
 
   return (
     <div className="h-screen flex flex-col justify-between bg-sn-bg-light-blue">
@@ -33,14 +61,16 @@ const GameSettings = () => {
             These roles are positions, distinct from players, that require someone to fill them during every game of the team.
             </h3>
             
-            <PersonTag name="Referee" number="null" isPlayer={false} isMember={true} />
-            <RoleInput/>
+            {roles.map((role, index) => (
+            <PersonTag key={index} name={role.name} number={role.number} isPlayer={role.isPlayer} isMember={role.isMember} />
+          ))}            
+          <RoleInput onAdd={addRole}/>
         </div>
     </div>
 
 
       <div className="bg-sn-bg-light-blue flex flex-col   align-items text-center pl-[15%]">
-        <button className="bg-sn-main-orange text-2xl text-white font-interElight p-2 rounded-10px w-[70vw] h-16 ">
+        <button onClick={saveData} className="bg-sn-main-orange text-2xl text-white font-interElight p-2 rounded-10px w-[70vw] h-16 ">
           SAVE
         </button>
 
