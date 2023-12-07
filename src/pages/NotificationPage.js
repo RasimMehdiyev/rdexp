a0c14648f9c39d81c678537ad7e6db5c553b9ec6
yeskamuhammad.omar <yeskamuhammad.omar@student.kuleuven.be>
2023-12-07 12:20:12 +0100

    further visualization

--- src/pages/NotificationPage.js
+++ src/pages/NotificationPage.js
@@ -54,28 +54,48 @@ const NotificationPage = () => {
     }, []);
 
     return (
-        <div className='flex-col gap-6'>
+        <div className='h-full w-full flex flex-col gap-4'>
             <div className='text-xl font-russoOne text-game-blue'>Team invites</div>
-            <div className='flex-col gap-4'>
-                {teamInvites && teamInvites.map((teamInvite) => (
-                    <div key={teamInvite.id } className='flex-col gap-4'>
-                        <div className='text-sm font-interReg'>{teamInvite.id}</div>
-                        <div className='text-sm font-interReg'>{teamInvite.title}</div>
-                        <div className='text-sm font-interReg'>{teamInvite.datetime}</div>
-                    </div>       
-                )
-                )}
+            <div className='flex flex-col gap-4'>
+                {teamInvites ?
+                    teamInvites.map((teamInvite) => (
+                    <div key={teamInvite.id }>
+                        <div className='flex flex-col gap-1'>
+                            <div className='text-sm font-interReg'>{teamInvite.id}</div>
+                            <div className='text-sm font-interReg'>{teamInvite.team_name}</div>
+                            <div className='text-sm font-interReg'>{teamInvite.club_name}</div>
+                        </div>
+                        <div className='flex flex-row gap-8'>
+                            <button>Accept</button>
+                            <button>Decline</button>
+                        </div>
+                    </div>
+                    )) :
+                    (<div className='text-sm font-interreg'>You have no team invite</div>)
+                }
+                
             </div>
             <div className='text-xl font-russoOne text-game-blue'>Event invites</div>
-            <div className='flex-col gap-4'>
-                {eventInvites && eventInvites.map((eventInvite) => (
-                    <div key={eventInvite.id } className='flex-col gap-4'>
-                        <div className='text-sm font-interReg'>{eventInvite.id}</div>
-                        <div className='text-sm font-interReg'>{eventInvite.title}</div>
-                        <div className='text-sm font-interReg'>{eventInvite.datetime}</div>
-                    </div>       
-                )
-                )}
+            <div className='flex flex-col gap-4'>
+                {eventInvites ?                    
+                    eventInvites.map((eventInvite) => (
+                        <div key={eventInvite.id }>
+                        <div className='flex flex-col gap-1 bg-sn-bg-light-blue rounded-md'>
+                            <div className='text-sm font-interReg'>{eventInvite.id}</div>
+                            <div className='text-sm font-interReg'>{eventInvite.title}</div>
+                            <div className='text-sm font-interReg'>{eventInvite.datetime}</div>
+                        </div>
+                        <div className='flex flex-row gap-8'>
+                            <button>Accept</button>
+                            <button>Decline</button>
+                        </div>
+                        </div>
+                    )
+                    ) : (
+                        <div className='text-sm font-interreg'>You have no event invite</div>
+                    )
+                }
+                
             </div>
         </div>
     )
