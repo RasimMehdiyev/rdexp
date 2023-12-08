93cc97debe99fd9e9bfa06a998c6f453c9580ebe
Penelope Rekkas <penelope.rekkas@student.kuleuven.be>
2023-12-08 12:26:27 +0100

    test validation RegisterPage in progress

--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -14,7 +14,34 @@ function RegisterPage() {
     const [role, setRole] = useState('0');
     const [password, setPassword] = useState('');
     const [confirmPassword, setConfirmPassword] = useState('');
+    const [emailError, setEmailError] = useState('');
+
     const navigate = useNavigate();
+
+    const validateEmail = (inputEmail) => {
+        const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return emailPattern.test(inputEmail);
+      };
+    
+      const handleBlur = () => {
+        if (email.trim() === '') {
+          setEmailError('');
+        } else if (!validateEmail(email)) {
+            setEmailError('Please enter a valid email address');
+            toast.error("Please enter a valid email address.", {
+                position: "top-center",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: false,
+                pauseOnHover: false,
+                draggable: true,
+                progress: undefined,
+                theme: "light",
+                });
+        } else {
+          setEmailError('');
+        }
+      };
 
 
     const handleSubmit = async (event) => {
@@ -55,30 +82,57 @@ function RegisterPage() {
         <div className='min-h-screen pt-10 pb-10 bg-sn-bg-light-blue flex flex-col gap-8 justify-center items-center'>
             <SynthleteLogo />
             <form className='text-center gap-2 w-80 items-center flex flex-col justify-center' onSubmit={handleSubmit}>
-                    <input className='shadow-md text-[12px] pl-2 font-interReg w-full h-12 rounded-lg border-2 border-sn-main-orange' placeholder='Full Name' type="text" value={fullName} onChange={(event) => setFullName(event.target.value)} />
-                    <input className='shadow-md text-[12px] pl-2 font-interReg w-full h-12 rounded-lg border-2 border-sn-main-orange' placeholder='Email' type="email" value={email} onChange={(event) => setEmail(event.target.value)} />
+                    <input
+                        className='shadow-md text-[12px] pl-2 font-interReg w-full h-12 rounded-lg border-2 border-club-header-blue'
+                        placeholder='Full Name'
+                        type="text"
+                        value={fullName}
+                        onChange={(event) => {
+                            const capitalizedFullName = event.target.value
+                            .split(' ')
+                            .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+                            .join(' ');
+
+                            setFullName(capitalizedFullName);
+                        }}
+                    />
+                    
+                    <input
+                    className={`shadow-md text-[12px] pl-2 font-interReg w-full h-12 rounded-lg border-2 ${
+                        emailError ? 'border-red-500' : 'border-club-header-blue'
+                      } ${
+                        emailError ? 'text-red-500' : '' 
+                      }`}
+                    placeholder='Email'
+                    type="email"
+                    value={email}
+                    onChange={(event) => setEmail(event.target.value)}
+                    onBlur={handleBlur}
+                    />
+                    
+
                     <PhoneInput
                     style={{ height: '3rem' }}
                     inputStyle={{ height: '100%', width:'100%' }}
-                    className='phone-input border-2 rounded-lg border-sn-main-orange'
+                    className='phone-input border-2 rounded-lg border-club-header-blue'
                     placeholder='Enter phone number'
                     value={phoneNumber}
                     onChange={(newPhoneNumber) => setPhoneNumber(newPhoneNumber)}
                     />
                     
-                    <input className='shadow-md pl-2 font-interReg w-full h-12 mt-5 rounded-lg border-2 border-sn-main-orange' placeholder='Password' type="password" value={password} onChange={(event) => setPassword(event.target.value)} />
-                    <input className='shadow-md pl-2 font-interReg w-full h-12 rounded-lg border-2 border-sn-main-orange' placeholder='Confirm Password' type="password" value={confirmPassword} onChange={(event) => setConfirmPassword(event.target.value)} />
+                    <input className='shadow-md pl-2 font-interReg w-full h-12 mt-5 rounded-lg border-2 border-club-header-blue' placeholder='Password' type="password" value={password} onChange={(event) => setPassword(event.target.value)} />
+                    <input className='shadow-md pl-2 font-interReg w-full h-12 rounded-lg border-2 border-club-header-blue' placeholder='Confirm Password' type="password" value={confirmPassword} onChange={(event) => setConfirmPassword(event.target.value)} />
                     {isPasswordMatch ? null : 
                         <p className='text-[10px] font-interReg leading-none text-[#ff0000d3] font-bold'>Passwords do not match!</p>
                     }
 
-                    <select className='shadow-md pl-2 font-interReg text-lf-dark-gray text-[12px] w-full h-9 mt-5 rounded-lg border-2 border-sn-main-orange' value={role} onChange={(event) => setRole(event.target.value)}>
+                    <select className='shadow-md pl-2 font-interReg text-lf-dark-gray text-[12px] w-full h-9 mt-5 rounded-lg border-2 border-club-header-blue' value={role} onChange={(event) => setRole(event.target.value)}>
                         <option className='font-interReg' value="0">Choose role</option>
                         <option className='font-interReg' value='1'>Coach</option>
                         <option className='font-interReg' value="2">Player</option>
                         <option className='font-interReg' value="3">Volunteer</option>
                     </select>
-                <button className={`${(isDisabled) ? 'cursor-not-allowed' : 'cursor-pointer'} text-white w-full h-16 mt-10 bg-sn-main-orange font-russoOne rounded-10px`}  type="submit">SIGN UP</button>
+                <button className={`${(isDisabled) ? 'cursor-not-allowed' : 'cursor-pointer'} text-white w-full h-16 mt-10 bg-club-header-blue font-russoOne rounded-10px`}  type="submit">SIGN UP</button>
                 <p className='py-2 text-xs text-sn-main-blue font-interReg'>Already have an account? <Link className='font-interReg font-bold text-sn-main-blue underline hover:text-[gray]' to="/login">Log in</Link></p>
             </form>
             <ToastContainer />
