22efea67c4179f49fafb9a8f7977a26e6da37eb4
Penelope Rekkas <penelope.rekkas@student.kuleuven.be>
2023-12-09 23:42:31 +0100

    Location filter on AboutClubPage done

--- src/components/LocationInput.js
+++ src/components/LocationInput.js
@@ -1,14 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import { useCombobox } from 'downshift';
 
-const LocationInput = () => {
+const LocationInput = ({ handleLocationInputChange }) => {
   const [inputItems, setInputItems] = useState([]);
   const [loading, setLoading] = useState(false);
 
-  const { isOpen, inputValue, getMenuProps, getInputProps, getItemProps, selectedItem } = useCombobox({
+  const {
+    isOpen,
+    inputValue,
+    getMenuProps,
+    getInputProps,
+    getItemProps,
+    selectedItem,
+  } = useCombobox({
     items: inputItems,
     onInputValueChange: ({ inputValue }) => {
-      // Fetch and update the list of addresses based on user input
       setLoading(true);
       fetchAddresses(inputValue).then((addresses) => {
         setInputItems(addresses);
@@ -18,11 +24,11 @@ const LocationInput = () => {
   });
 
   const fetchAddresses = async (query) => {
-    // Implement your address fetching logic here, for example, using an API like OpenCage
-    // This is a simplified example; you'll need to replace it with your actual API calls
-    const apiKey = '22ff9dc36a8a460893811d86a9e2436c';
+    const apiKey = '22ff9dc36a8a460893811d86a9e2436c'; 
     const response = await fetch(
-      `https://api.opencagedata.com/geocode/v1/json?q=${encodeURIComponent(query)}&key=${apiKey}`
+      `https://api.opencagedata.com/geocode/v1/json?q=${encodeURIComponent(
+        query
+      )}&key=${apiKey}`
     );
     const data = await response.json();
 
@@ -34,9 +40,30 @@ const LocationInput = () => {
   };
 
   return (
-    <div>
-      <input {...getInputProps()} placeholder="Enter address" />
-      <ul {...getMenuProps()} style={{ listStyle: 'none', padding: 0 }}>
+    <div className='input-container'>
+      
+        <div className="realtive w-full">
+            <img className="input-icon pt-1" src={process.env.PUBLIC_URL + "/images/map-pin.svg"} />
+            <textarea
+            {...getInputProps()}
+            type="text"
+            placeholder='Location'
+            className="text-black border-2 pl-8 border-club-header-blue h-30 rounded-10px min-w-full m-auto"
+            maxLength={255}
+            rows={4}
+            />
+        </div>
+      <ul
+        {...getMenuProps()}
+        style={{
+          listStyle: 'none',
+          padding: 0,
+          position: 'absolute',
+          top: '100%', // Display below the input
+          left: 0,
+          zIndex: 1,
+        }}
+      >
         {isOpen &&
           (loading ? (
             <li>Loading...</li>
