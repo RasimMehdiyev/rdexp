0e47e2fab1b3e2e8cd173dda76db4c1efb9773de
Lukas Dewijngaert <lukas.dewijngaert@student.kuleuven.be>
2023-12-13 17:34:06 +0100

    buttons added to the eventcard

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -348,11 +348,6 @@ else {
                     
                   ))}
               </div>
-              <div className="absolute relative inline-flex bottom-[85px] z-10000  text-[15px] font-extralight font-['Inter'] left-[295px] text-[#485687] ">Attend?</div>
-                <label className="relative inline-flex  items-center cursor-pointer left-[245px] bottom-[55px]">
-                  <input type="checkbox" value="" className="sr-only peer" style={{ outline: 'none' }} />
-                  <div className="w-11 h-6  peer-focus:outline-none  rounded-full peer dark:bg-[#c2272e] peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-[#06b80f]"></div>
-                </label>
             </div>
             
           ))}
