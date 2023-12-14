1d5d89c782290b52931fb1f6c8bbd8d28728a676
Rasim Mehdiyev <rasim.mehdiyev@student.kuleuven.be>
2023-12-14 10:37:04 +0100

    fixed header in event overview

--- src/components/StickySubheaderEventCreateComponent.js
+++ src/components/StickySubheaderEventCreateComponent.js
@@ -7,10 +7,10 @@ import { useNavigate } from 'react-router-dom';
 const StickySubheaderEventCreateComponent = ({onSave,buttonEnabled,eventType}) => {
 
     const eventTypes = () => {
-        if(eventType === "Game"){
+        if(eventType === "game"){
             return "Game"
         }
-        else if(eventType === "Practice"){
+        else if(eventType === "practice"){
             return "Practice"
         }
         else{
